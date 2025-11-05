556a65c829f1db32fcd8e761db55625f36b5c9e2
MohamedSadat <sadat.vb@gmail.com>
2025-11-06 01:08:07 +0200

    add ReturnWinner

--- contracts/votecontract.sol
+++ contracts/votecontract.sol
@@ -25,15 +25,7 @@ contract VotingSystem {
         _; // This special symbol means "run the function code"
     }
 
-    // 4. Functions (The Logic)
-    
-    /**
-     * @notice Stores a new number, but only the owner can call it.
-     * @param _newNumber The number to store
-     */
-    function storeNumber(uint256 _newNumber) public onlyOwner {
-        favoriteNumber = _newNumber;
-    }
+
      function reset() public onlyOwner {
         candidate1 = 0;
         candidate2 = 0;
@@ -41,17 +33,17 @@ contract VotingSystem {
 
     }
 
-        function voteFirst(uint256 _newNumber) public
+        function voteFirst() public
          {
-        candidate1 = _newNumber;
+        candidate1++;
          }
-    function voteSecond(uint256 _newNumber) public
+    function voteSecond() public
          {
-        candidate2 = _newNumber;
+        candidate2++;
          }
-         function voteThird(uint256 _newNumber) public
+         function voteThird() public
          {
-        candidate3 = _newNumber;
+        candidate3++;
          }
          
     /**
@@ -69,5 +61,41 @@ contract VotingSystem {
     }
             function retrieveTotal() public view returns (uint256) {
         return candidate1+candidate2+candidate3;
+    }
+    function getWinner() public view returns (string memory) {
+        uint256 c1 = candidate1;
+        uint256 c2 = candidate2;
+        uint256 c3 = candidate3;
+
+        // Check for Candidate 1 being the winner
+        if (c1 > c2 && c1 > c3) {
+            return "Candidate 1";
+        } 
+        // Check for Candidate 2 being the winner
+        else if (c2 > c1 && c2 > c3) {
+            return "Candidate 2";
+        } 
+        // Check for Candidate 3 being the winner
+        else if (c3 > c1 && c3 > c2) {
+            return "Candidate 3";
+        } 
+        // Check for a three-way tie
+        else if (c1 == c2 && c2 == c3) {
+            return "Three-way Tie!";
+        } 
+        // Check for a two-way tie
+        else if (c1 == c2) {
+            return "Tie between Candidate 1 and Candidate 2";
+        }
+        else if (c1 == c3) {
+            return "Tie between Candidate 1 and Candidate 3";
+        }
+        else if (c2 == c3) {
+            return "Tie between Candidate 2 and Candidate 3";
+        } 
+        // Fallback for an unhandled scenario (shouldn't happen with the logic above)
+        else {
+            return "No clear winner determined.";
+        }
     }
 }
