66c0d4d2e10831bdb28b14eef2c9aaa7e11b6f84
MohamedSadat <sadat.vb@gmail.com>
2025-11-11 12:46:12 +0200

    Enumeration

--- contracts/VoucherSystem.sol
+++ contracts/VoucherSystem.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.5.0
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.24;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract CGVoucher is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract CGVoucher is ERC721, ERC721URIStorage, ERC721Burnable,ERC721Enumerable, Ownable {
     uint256 private _nextTokenId;
+
+event VoucherMinted(address indexed to, uint256 indexed tokenId, string uri);
 
     constructor(address initialOwner)
         ERC721("CG Voucher", "VCH")
@@ -27,8 +30,16 @@ contract CGVoucher is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        emit VoucherMinted(to, tokenId, uri);
+
+
         return tokenId;
     }
+    function burn(uint256 tokenId) public override {
+    require(ownerOf(tokenId) == msg.sender, "Not owner");
+    // Optional: Add redemption check, e.g., !isRedeemed[tokenId]
+    super.burn(tokenId);
+}
 
     // The following functions are overrides required by Solidity.
 
@@ -44,9 +55,26 @@ contract CGVoucher is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721, ERC721URIStorage,ERC721Enumerable)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
+
+     function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+
 }
